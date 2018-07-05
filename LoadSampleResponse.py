2528455c252198bbeeff6615673b7ed6f1af9246
faisalshafi39 <shafi.faisal@bqe.com>
2018-07-05 14:18:32 +0530

    Added Script relatedf to Sample Response and Added Json File.

---
+++ LoadSampleResponse.py
@@ -0,0 +1,29 @@
+import json
+from pprint import pprint
+
+
+# Load Json into memory
+with open('C:\Users\Faisal.Shafi\Desktop\\SampleResponse.Json') as f:
+    
+    data = json.load(f)
+
+import pyodbc 
+import re
+
+# Creating SQL SERVER Creation
+cnxn = pyodbc.connect(r"Driver={SQL Server Native Client 11.0};Server=.\SQL2016;Database=PublicApi;uid=sa;pwd=sa")
+						
+cursor = cnxn.cursor()
+
+
+# for values in endpoints:
+for endname in data:
+    cursor.execute("select APIID,ID,VerbID from endpoints where Name=? ",endname['endpoint'])
+    endpoints = cursor.fetchone()
+   
+    print endpoints
+    if endpoints is not None:
+        cursor.execute("Update Endpoints set RequestURI=? , SampleResponse=? where APIID=? and ID=?",endname['url'],endname['sample'],endpoints[0],endpoints[1])
+        cursor.commit()
+
+    # print values
